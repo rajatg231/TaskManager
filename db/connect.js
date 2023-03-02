7ed11e370441d0c66dec6d9d263ab2be05ed6159
Rajat <rajatg231@users.noreply.github.com>
2023-03-02 17:56:39 +0530

    added .env.example and updated readme

--- db/connect.js
+++ db/connect.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 
 const connectDB = (url) => {
   return mongoose.connect(url, {
+    authSource: "admin",
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
